d88c996ed8cdbc000a1a220396be37f1fe93d4d0
Florian Schwab <me@ydkn.de>
2010-04-20 11:04:49 +0200

    Extended test coverage

--- vendor/plugins/git_provider/test/git_provider_test.rb
+++ vendor/plugins/git_provider/test/git_provider_test.rb
@@ -17,32 +17,111 @@
 require 'test_helper'
 
 class GitProviderTest < Test::Unit::TestCase
+
   # Setup a GIT repository for testing
   def setup
-    `git init #{Rails.root}/tmp/test/git-repo`
-    @provider = GitProvider.new({'path' => "#{Rails.root}/tmp/test/git-repo"})
+    FileUtils.remove_dir("#{Rails.root}/tmp/test-git-repo", true) if File.exists?("#{Rails.root}/tmp/test-git-repo")
+    `git init #{Rails.root}/tmp/test-git-repo`
+    Dir.mkdir("#{Rails.root}/tmp/test-git-repo/pages")
+    @provider = GitProvider.new({'path' => "#{Rails.root}/tmp/test-git-repo"})
   end
 
   # Remove GIT repository after testing
   def teardown
-    `rm -rf #{Rails.root}/tmp/test/git-repo`
+    FileUtils.remove_dir("#{Rails.root}/tmp/test-git-repo", true)
   end
 
   # Create a new page
   def test_create_page
     page_name = 'TestPage'
-    user = User.new(:username => 'TestUser', :email => 'test@user.com')
-    @provider.save_page(page_name, "test content 123", user, 'changed')
-    assert(@provider.page_exists?(page_name), "Failed to created page.")
+    content = "test content 123"
+    page_save page_name, content, 'changed', default_user
+    assert page_exists?(page_name)
+    assert_equal content, page_contents(page_name)
+    revisions = page_revisions page_name
+    assert_equal 1, revisions.length
+    assert_equal default_user.username, revisions[0].user
   end
 
   # Update page contents
   def test_update_page
-    page_name = 'TestPage'
-    user = User.new(:username => 'TestUser', :email => 'test@user.com')
-    old_content = @provider.page_contents(page_name)
-    @provider.save_page(page_name, "987 test content", user, 'changed')
-    new_content = @provider.page_contents(page_name)
-    assert(new_content != old_content, "Failed to update page.")
+    page_name = 'TestPage2'
+    old_content = "content"
+    page_save page_name, old_content, 'create', default_user
+    assert_equal old_content, page_contents(page_name)
+    new_content = "new content"
+    update_user = user 'updater', 'updater@dom.org'
+    page_save page_name, new_content, 'update', update_user
+    assert_equal new_content, page_contents(page_name)
+    revisions = page_revisions page_name
+    assert_equal 2, revisions.length
+    assert_equal default_user.username, revisions[0].user
+    assert_equal update_user.username, revisions[1].user
   end
+
+  # Rename page
+  def test_rename_page
+    old_page = 'TestPage'
+    new_page = 'TestPageRenamed'
+    page_save old_page, "some content", 'create', default_user
+    content = page_contents old_page
+    rename_user = user 'renamer', 'renamer@dom.org'
+    page_rename old_page, new_page, rename_user
+    assert !page_exists?(old_page)
+    assert page_exists?(new_page)
+    assert_equal content, page_contents(new_page)
+    revisions = page_revisions new_page
+    assert_equal 1, revisions.length
+    assert_equal rename_user.username, revisions[0].user
+  end
+
+  # Delete a existing page
+  def test_delete_page
+    page_name = 'TestPageToDelete'
+    page_save page_name, "some content", 'create', default_user
+    delete_user = user 'deleter', 'deleter@dom.org'
+    page_delete page_name, delete_user
+    assert !page_exists?(page_name)
+  end
+
+  private
+
+  # Creates a user
+  def user(username, email)
+    u = User.new
+    u.username = username
+    u.email = email
+    u
+  end
+
+  # Default user
+  def default_user
+    @default_user = user('testuser', 'test@user.org') if @default_user.nil?
+    @default_user
+  end
+
+  def page_exists?(name)
+    @provider.page_exists? name
+  end
+
+  def page_contents(name)
+    @provider.page_contents name
+  end
+
+  def page_revisions(name)
+    @provider.page_revisions name
+  end
+
+  def page_save(name, content, message, user)
+    @provider.page_save name, content, message, user
+  end
+
+  def page_rename(old_name, new_name, user)
+    @provider.page_rename old_name, new_name, user
+  end
+
+  def page_delete(name, user)
+    @provider.page_delete name, user
+  end
+
 end
