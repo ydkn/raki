96b8d55ce34fbed8d3483b6a4bdcb1eb4f49d184
Florian Schwab <me@ydkn.de>
2010-04-29 10:43:30 +0200

    Fixed sorting problem with ruby 1.8

--- vendor/plugins/index_plugin/init.rb
+++ vendor/plugins/index_plugin/init.rb
@@ -27,15 +27,14 @@ Raki::Plugin.register :index do
   execute do |params, body, context|
     rnd = rand(900)+100
     letters = {}
-    pages = Raki.provider(:page).page_all
-    pages = pages.sort { |a,b| a <=> b }
-    pages.each do |page|
+    Raki.provider(:page).page_all.sort.each do |page|
       letter = page[0].chr.upcase
       letters[letter] = [] unless letters.key?(letter)
       letters[letter] << page
     end
     header = ""
     body = ""
+    letters = letters.sort { |a,b| a <=> b }
     letters.each do |letter, value|
       header += "<a href=\"#INDEX-#{letter}-#{rnd}\">#{letter}</a> - "
       body += "<div class=\"index_letter\" id=\"INDEX-#{letter}-#{rnd}\">#{letter}</div><div class=\"index_pages\">"
