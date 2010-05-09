09fa394d45545fa034e8416b43b1c14a345ffd62
Florian Schwab <me@ydkn.de>
2010-05-09 23:25:14 +0200

    Filesize formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,5 +27,26 @@ module ApplicationHelper
     end
     stylesheets.join("")
   end
+  
+  KILOBYTE = 1024.0
+  MEGABYTE = 1048576.0
+  GIGABYTE = 1073741824.0
+
+  def format_size(size)
+    size = size.to_f
+    case
+      when size == 1
+        out = "1 Byte"
+      when size < KILOBYTE
+        out = "#{size} Bytes"
+      when size < MEGABYTE
+        out = "%.2f KB" % (size/KILOBYTE)
+      when size < GIGABYTE
+        out = "%.2f MB" % (size/MEGABYTE)
+      else
+        out = "%.2f GB" % (size/GIGABYTE)
+    end
+    out
+  end
 
 end
