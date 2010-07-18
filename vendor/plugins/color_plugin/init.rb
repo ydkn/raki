22c2ce5fd8419e8f211e8674c4b9956da3c5b06e
Florian Schwab <me@ydkn.de>
2010-07-18 13:29:44 +0200

    Added parsing of body

--- vendor/plugins/color_plugin/init.rb
+++ vendor/plugins/color_plugin/init.rb
@@ -1,3 +1,19 @@
+# Raki - extensible rails-based wiki
+# Copyright (C) 2010 Florian Schwab & Martin Sigloch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 Raki::Plugin.register /^red|blue|green|yellow|grey|black|white|[0-9a-f]{3}|[0-9a-f]{6}$/i do
   
   name 'Color Plugin'
@@ -18,9 +34,9 @@ Raki::Plugin.register /^red|blue|green|yellow|grey|black|white|[0-9a-f]{3}|[0-9a
       }
       
     if callname.to_s =~ /^[0-9a-f]{3}|[0-9a-f]{6}$/i
-      "<font style=\"color:##{callname.to_s};\">#{body}</font>"
+      "<font style=\"color:##{callname.to_s};\">#{Raki.parser(context[:type]).parse body}</font>"
     elsif colors.key? callname.to_sym
-      "<font style=\"color:##{colors[callname.to_sym]};\">#{body}</font>"
+      "<font style=\"color:##{colors[callname.to_sym]};\">#{Raki.parser(context[:type]).parse body}</font>"
     else
       raise Raki::Plugin::PluginError.new t 'color.invalid_color'
     end
