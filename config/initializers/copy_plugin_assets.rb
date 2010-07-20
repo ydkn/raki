a3de7097396ef0e23290559431b440ea4a1f7e98
Florian Schwab <me@ydkn.de>
2010-07-20 18:12:06 +0200

    Temporary fix for copying plugin assets

--- config/initializers/copy_plugin_assets.rb
+++ config/initializers/copy_plugin_assets.rb
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+FileUtils.rm_rf(File.join(Rails.root, 'public', 'plugin_assets'))
 FileUtils.mkdir_p(File.join(Rails.root, 'public', 'plugin_assets'))
 
 Dir[File.join(Rails.root, 'vendor', 'plugins', '*')].each do |plugin|
