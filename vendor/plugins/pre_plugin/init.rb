18f6c83ce862098f64f41ce518663f03c7cce5b5
f0i <f0i.git@projects.f0i.de>
2010-07-29 23:55:24 +0200

    Plugin for preformated text

---
+++ vendor/plugins/pre_plugin/init.rb
@@ -0,0 +1,29 @@
+# Raki - extensible rails-based wiki
+# Copyright (C) 2010 Florian Schwab & Martin Sigloch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Raki::Plugin.register :pre do
+
+  name 'Pre_Plugin'
+  description 'Plugin for preformatted text'
+  url 'http://github.com/ydkn/raki'
+  author 'Martin Sigloch'
+  version '0.1'
+
+  execute do
+    "<pre>#{h body}</pre>"
+  end
+
+end
