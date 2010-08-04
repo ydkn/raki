d3e0b8ad454b52dd8612a1da9cce61a59fa8248d
Florian Schwab <me@ydkn.de>
2010-08-05 01:09:25 +0200

    Extra methods for provider and parser

--- lib/raki/plugin_helpers.rb
+++ lib/raki/plugin_helpers.rb
@@ -21,82 +21,90 @@ module Raki
       !(url.match "^[a-zA-Z]+:\/\/(.+(:.+)?@)?[a-zA-Z0-9_-](\.[a-zA-Z0-9_-])*(:[0-9]+)?/").nil?
     end
     
+    def provider(type)
+      Raki.provider(type)
+    end
+    
     def page_exists?(type, name, revision=nil)
-      Raki.provider(type).page_exists?(type, name, revision)
+      provider(type).page_exists?(type, name, revision)
     end
 
     def page_contents(type, name, revision=nil)
-      Raki.provider(type).page_contents(type, name, revision)
+      provider(type).page_contents(type, name, revision)
     end
 
     def page_revisions(type, name)
-      Raki.provider(type).page_revisions(type, name)
+      provider(type).page_revisions(type, name)
     end
 
     def page_save(type, name, contents, message, user=User.current)
-      Raki.provider(type).page_save(type, name, contents, message, user)
+      provider(type).page_save(type, name, contents, message, user)
     end
 
     def page_rename(old_type, old_name, new_type, new_name, user=User.current)
-      Raki.provider(type).page_rename(old_type, old_name, new_type, new_name, user)
+      provider(type).page_rename(old_type, old_name, new_type, new_name, user)
     end
 
     def page_delete(type, name, user=User.current)
-      Raki.provider(type).page_delete(type, name, user)
+      provider(type).page_delete(type, name, user)
     end
 
     def page_all(type)
-      Raki.provider(type).page_all(type)
+      provider(type).page_all(type)
     end
 
     def page_changes(type, amount=0)
-      Raki.provider(type).page_changes(type, amount)
+      provider(type).page_changes(type, amount)
     end
     
     def page_diff(type, page, revision_from=nil, revision_to=nil)
-      Raki.provider(type).page_diff(type, page, revision_from, revision_to)
+      provider(type).page_diff(type, page, revision_from, revision_to)
     end
 
     def attachment_exists?(type, page, name, revision=nil)
-      Raki.provider(type).attachment_exists?(type, page, name, revision)
+      provider(type).attachment_exists?(type, page, name, revision)
     end
 
     def attachment_contents(type, page, name, revision=nil)
-      Raki.provider(type).attachment_contents(type, page, name, revision)
+      provider(type).attachment_contents(type, page, name, revision)
     end
 
     def attachment_revisions(type, page, name)
-      Raki.provider(type).attachment_revisions(type, page, name)
+      provider(type).attachment_revisions(type, page, name)
     end
 
     def attachment_save(type, page, name, contents, message, user=User.current)
-      Raki.provider(type).attachment_save(type, page, name, contents, message, user)
+      provider(type).attachment_save(type, page, name, contents, message, user)
     end
 
     def attachment_delete(type, page, name, user=User.current)
-      Raki.provider(type).attachment_delete(type, page, name, user)
+      provider(type).attachment_delete(type, page, name, user)
     end
 
     def attachment_all(type, page)
-      Raki.provider(type).attachment_all(type, page)
+      provider(type).attachment_all(type, page)
     end
 
     def attachment_changes(type, page=nil, amount=nil)
-      Raki.provider(type).attachment_changes(type, page, amount)
+      provider(type).attachment_changes(type, page, amount)
     end
     
     def types
       types = []
       Raki.initialized_providers.values.each do |provider|
         provider.types.each do |type|
-          types << type if Raki.provider(type) == provider
+          types << type if provider(type) == provider
         end
       end
       types
     end
     
+    def parser(type)
+      Raki.parser(type)
+    end
+    
     def parse(type, content, context=context)
-      Raki.parser(type).parse(content, context)
+      parser(type).parse(content, context)
     end
     
     def parsed_page(type, page, revision=nil, context=context)
