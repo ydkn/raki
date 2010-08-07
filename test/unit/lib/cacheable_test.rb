35a423c3a2857ef8ee2df6cfded0939fc03058fc
Florian Schwab <me@ydkn.de>
2010-08-07 15:04:53 +0200

    Unit test for primary functionality

--- test/unit/lib/cacheable_test.rb
+++ test/unit/lib/cacheable_test.rb
@@ -1,5 +1,5 @@
 # Raki - extensible rails-based wiki
-# Copyright (C) 2010 Florian Schwab
+# Copyright (C) 2010 Florian Schwab & Martin Sigloch
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,10 +14,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-one:
-  username: user1
-  email: user1@user1.dom
+require 'test_helper'
 
-two:
-  username: user2
-  email: user2@user2.dom
+class UserTest < Test::Unit::TestCase
+  
+  class TestCache
+    include Cacheable
+    def cached_method
+      @test.nil? ? @test = 0 : @test += 1
+      @test
+    end
+    cache :cached_method, :ttl => 3
+  end
+  
+  def test_refresh
+    cached = TestCache.new
+    5.times do |i|
+      assert_equal i, cached.cached_method
+      sleep 1
+      assert_equal i, cached.cached_method
+      sleep 2
+      assert_equal i, cached.cached_method
+      sleep 1
+    end
+  end
+  
+end
