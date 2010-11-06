9da071c9bed37aca7553b3d30101eb27e215ce4b
Florian Schwab <me@ydkn.de>
2010-11-07 00:24:09 +0100

    Basic toolbar functionality

---
+++ public/javascripts/application.js
@@ -0,0 +1,41 @@
+/* 
+ * Raki - extensible rails-based wiki
+ * Copyright (C) 2010 Florian Schwab & Martin Sigloch
+ */
+
+function toolbarClickEvent(event) {
+	toolbarItem = event.srcElement.parentNode;
+	itemAttributes = toolbarItem.attributes;
+	
+	var content = document.getElementById('content');
+	var selectedText = content.value.substring(content.selectionStart, content.selectionEnd);
+
+	var params = new Object();
+	for(i = 0; i < itemAttributes.length; i++) {
+		attrName = toolbarItem.attributes[i].name;
+		attrValue = toolbarItem.attributes[i].value;
+		if(attrName.match("^data-")) {
+			params[attrName.substring(5, attrName.length)] = attrValue;
+		}
+	}
+	
+	newSelectedText = null;
+	
+	if(params['enclosed']) {
+		newSelectedText = params['enclosed'] + selectedText + params['enclosed'];
+	}
+
+	content.value = content.value.substring(0, content.selectionStart) + newSelectedText + content.value.substring(content.selectionEnd, content.value.length);
+	
+}
+
+function initToolbar() {
+    toolbarItems = document.getElementById("toolbar-items");
+	if(toolbarItems != null) {
+		children = toolbarItems.children;
+		for(i = 0; i < children.length; i++) {
+			children[i].onclick = toolbarClickEvent;
+		}
+	}
+}
+window.onload = initToolbar;
