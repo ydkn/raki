a5d6c5e33e8676d17058fcbd432eab28d2917a4f
Florian Schwab <me@ydkn.de>
2010-11-06 15:07:11 +0100

    Only show breadcrumbs if not empty

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  # Scrub sensitive parameters from your log
   filter_parameter_logging :password
 
   helper AuthenticationHelper
