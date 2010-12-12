63decf75102dc567ceb297310af33a604c72425b
Florian Schwab <me@ydkn.de>
2010-12-12 14:20:41 +0100

    Added basic mime_type tests

---
+++ test/unit/attachment_test.rb
@@ -0,0 +1,34 @@
+# Raki - extensible rails-based wiki
+# Copyright (C) 2010 Florian Schwab & Martin Sigloch
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+require 'test_helper'
+
+class AttachmentTest < Test::Unit::TestCase
+  
+  def test_mime_type
+    {
+      'test.png' => 'image/png',
+      'test.jpg' => 'image/jpeg',
+      'test.pdf' => 'application/pdf',
+      'test.foobar' => 'application/octet-stream',
+      nil => nil
+    }.each do |name, content_type|
+      a = Attachment.new :namespace => 'testNS', :page => 'testPAGE', :name => name
+      assert_equal content_type, a.mime_type
+    end
+  end
+  
+end
